22bfe5381441626fc3396560c265d64b1d837b6c
Keiler <keiler@Brasporto.local>
2026-06-19 17:53:24 -0300

    fix: Hub publico, senha apenas em cada sistema

--- middleware.ts
+++ middleware.ts
@@ -9,5 +9,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/hub/:path*', '/dashboard/:path*', '/admin/:path*'],
+  matcher: ['/dashboard/:path*', '/admin/:path*'],
 };
